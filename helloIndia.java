c9944542b346d59d744eb6eb17480206710c6a4a
Dheeraj Choudhary <30520332+Draj23@users.noreply.github.com>
2020-12-22 19:42:42 +0530

    Update helloIndia.java

--- helloIndia.java
+++ helloIndia.java
@@ -1,6 +1,16 @@
 class helloIndia{
 
+    public void demo(){
+    
+    System.out.println("demo function");
+    
+    }
+    
+    
     public static void main(String args[]){  
      System.out.println("Hello India");  
+        helloIndia sampledemo= new helloIndia();
+        sampledemo.demo();
+        
     }  
 }  
